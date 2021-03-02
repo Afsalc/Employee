9e0de1237ae9836d897ca99999833078f838f982
Afsal <techieck76@gmail.com>
2021-03-02 12:59:50 +0530

    Revert "Revert "Test_interview""
    This reverts commit 9e839d301646e7fd954b8e3e5b3e47b6a8d52d23.

---
+++ Employee/Employee_API/Models/MasterEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace Employee_API.Models
+{
+    public partial class MasterEmployee
+    {
+        public int PkempId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeDesigination { get; set; }
+        public DateTime EmployeeDob { get; set; }
+        public string EmployeePhonenumber { get; set; }
+    }
+}
